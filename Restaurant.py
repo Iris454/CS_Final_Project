13e3f0c8086a62248bf313b25bc6974dc25c1e1c
Hargun-K <hargunjit.kaur@rbe.sk.ca>
2025-05-27 09:04:15 -0600

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -1,16 +1,30 @@
 import Move as m
 
 def north():
-    pass
+    print("That's the elevator, to go there you have to solve this easy riddle!")
+    print("I have keys, but no locks, I have a space, but no room. You can enter, but can't go outside.")
+    answer = input("What am I?: ")
+    if answer == "keyboard" or "Keyboard": 
+    print("Congratulations, you have solved it, very intelligent.")
+    else:
+    print("No, that is wrong.")
+
 
 def south():
-    pass
+    print("Sorry there's nothing that way, pick another direction.")
 
 def east():
-    pass
+    print("That's the pool, to go there you have to solve this tricky riddle!")
+    print("Tim's mom has 4 sons, Red, Orange, and Yellow.")
+    answer = input("What is the fourth son's name?: ")
+    if answer == "Tim":
+        print("Yes, you're clever. Now you can go into the pool.")
+    else:
+        print("Wrong.")
+    
 
 def west():
-    pass
+    print("You're back in the lobby")
 
 dir = [north, south, east, west]
 
