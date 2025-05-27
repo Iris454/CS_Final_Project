64ea611df978742acffe9578022f8b0fb111f880
Iris454 <yingyan.xu1@rbe.sk.ca>
2025-05-27 09:25:25 -0600

    Finish filling in Lobby module

--- Bag.py
+++ Bag.py
@@ -1,1 +1,4 @@
 bag = []
+
+first_floor = [[None, "elevator", None], ["lobby", "restuarant", "pool"]]
+second_floor = [["elevator", "Room103", "Room104"], [None, "gym", None]]
--- Lobby.py
+++ Lobby.py
@@ -1,7 +1,11 @@
 import Move as m
 import Bag as b
+import Restaurant as r
+from tabulate import tabulate
 
 flag = True
+answer = 0
+pw = 1372
 
 def north():
     print("You are at the reception desk.")
@@ -12,11 +16,17 @@ def north():
         print("2. check in")
         choice = int(input("choice (1/2): "))
         if choice == 1:
-            pass #create a grid
-        elif choice == 2:
+            print("first floor:")
+            print(tabulate(b.first_floor, tablefmt = "fancy_grid"))
+            print("second floor:")
+            print(tabulate(b.second_floor, tablefmt = "fancy_grid"))
+            break
+        elif choice == 2 and len(b.bag) == 0:
             print("Sir, this is your room key. Your room is 103.")
             b.bag.append("Room103 Key")
             break
+        elif choice == 2 and len(b.bag) != 0:
+            print("Sir, you've already checked in. You don't need to do it again.")
         else: 
             print("Please choose 1 or 2.")
 
@@ -25,14 +35,26 @@ def south():
     print("This is the entrance. The door is locked. Come back here at the end to unlock it with a key.")
 
 def east():
+    global flag
     print("This is the door to the restaurant. You need a four-digit password to unlock it.")
     while True:
         print("1. enter password")
         print("2. look around in the lobby")
         choice = int(input("choice (1/2): "))
         if choice == 1:
-            pw = int(input("password: "))
-            # check if it matches the answer to the math question
+            if answer == 0:
+                print("You haven't found the password. Please walk around in the lobby to find it.")
+            elif answer == pw:
+                print("The password is correct!")
+                if len(b.bag) == 0:
+                    print("However, you haven't visited all the areas in the lobby.")
+                else:
+                    print("Now you may visit the restuarant.")
+                    flag = False
+                    r.restaurant()
+            else:
+                print("Incorrect password. Please solve the math question again.")
+            
             break
         elif choice == 2:
             break
@@ -41,8 +63,12 @@ def east():
 
 
 def west():
-    print("You see a math question: Two nonnegative numbers have a sum of 21. What is the maximum product of ")
-    pass
+    global answer
+    print("You see a math question: " \
+    "Two nonnegative numbers have a sum of 21. If the product of one of the numbers " \
+    "with the square of the other is to be a maximum, what is this maximum product?")
+    answer = int(input("answer: "))
+
 
 dir = [north, south, east, west]
 
