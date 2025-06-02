5b12f979fc4b730fb731e4f32ff3d4b6a444bb1b
Yingyan XU <yingyan.xu1@RBE.sk.ca>
2025-06-02 09:30:45 -0600

    Update Room2 (2)

--- Room2.py
+++ Room2.py
@@ -3,9 +3,24 @@ import Bag as b
 
 def room2():
     print("In this room, you only see a locked box with a four-digit password.")
-    choice = input("What do you do now?: ")
-    if "bag" in choice.lower().split():
-        b.check()
-        
+    while True:
+        choice = input("What do you do now?: ")
+        if "bag" in choice.lower().split():
+            while True:
+                b.check()
+                print("Which item do you want to use now?")
+                ans = int(input("choose a number: "))
+                if ans > 0 and ans <= len(b.bag):
+                    if b.bag[ans-1] == "UV Torch":
+                        print("Using the UV Torch, you see the hidden password: 3421.")
+                        print("Hooray, you get the last piece of key fragment! You escape the hotel!")
+                        break
+                    else:
+                        print("This item is not useful now. Please pick again.")
+                else:
+                    print("Pleae pick a number labeled in front of the items.")
+            break
+        else:
+            print("Hint: type out the thing that you have along the way.")
         
 
