537fbca05295cd5d183f3478c8f315160ea3d5bc
Hargun-K <hargunjit.kaur@rbe.sk.ca>
2025-06-12 08:57:56 -0600

    move descriptions to each file

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 
-descriptions = {"lobby": "A big room with a grand red carpet leading down to the reception desk." \
-                " Classy and sopisticated. It seems perfect at first glance, but with a closer look somthing seems off.",   
-                "pool": "A dull and dark pool area, with beach chairs set along the edge. The stack of unused towels in the corner, "\
-                "gives the sense that somthings wrong. When was the last time this pool was used? ", 
-                "restaurant": "The lights are dim, long shadows across the open space where tables are neatly arranged." \
-                        " Each set with folded napkins, polished dishes, and gleaming cutlery, prepared, but untouched." \
-                        " A crystal chandelier sways gently in a draft that seems to come from nowhere," \
-                        " though not a single window is open. The room is silent, as if time itself  is paused.", 
-                "elevator": "The floor creeks of the old elevator, there are only 2 buttons, 1 and 2. Be carefull, it might break down.", 
-                "room1": "Room 103. A faded scent of dust lingers in the air. The wallpaper peels revealing stained walls."\
-                 " A flickering ceiling light casts long shadows across the room's furniture." \
-                        "An unmade bed, a cracked vanity mirror, and a creaky armoire half-open.",
-                "room2": "Room 104. The room smells of smoke. A rusted ceiling fan turns slowly above, barely moving the air."\
-                         " Yellowed lace curtains hang over a single, cracked window, filtering the dim light from the street outside."\
-                        " An old rotary phone sits on the nightstand beside a chipped lamp. The bed is lumpy and uneven, its sheets faded."}
-"}
+
 
 import Lobby as l
 name = input("Hi, what's your name! ")
