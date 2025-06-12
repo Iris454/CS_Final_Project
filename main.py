02a8165700669fa11da6bba560aa96a8f960cfc8
Hargun-K <hargunjit.kaur@rbe.sk.ca>
2025-06-12 08:45:02 -0600

    Add room descriptions

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 
-descriptions = {"lobby": "A big room with a grand red carpet leading down to the reception desk. \n"
+descriptions = {"lobby": "A big room with a grand red carpet leading down to the reception desk." \
                 " Classy and sopisticated. It seems perfect at first glance, but with a closer look somthing seems off.",   
-                "pool": "A dull and dark pool area, with beach chairs set along the edge. The stack of unused towels in the corner, \n"
+                "pool": "A dull and dark pool area, with beach chairs set along the edge. The stack of unused towels in the corner, "\
                 "gives the sense that somthings wrong. When was the last time this pool was used? ", 
-                "restaurant": "The lights are dim, long shadows across the open space where tables are neatly arranged. \n" 
-                        " Each set with folded napkins, polished dishes, and gleaming cutlery, prepared, but untouched. \n" 
-                        " A crystal chandelier sways gently in a draft that seems to come from nowhere, \n"
+                "restaurant": "The lights are dim, long shadows across the open space where tables are neatly arranged." \
+                        " Each set with folded napkins, polished dishes, and gleaming cutlery, prepared, but untouched." \
+                        " A crystal chandelier sways gently in a draft that seems to come from nowhere," \
                         " though not a single window is open. The room is silent, as if time itself  is paused.", 
                 "elevator": "The floor creeks of the old elevator, there are only 2 buttons, 1 and 2. Be carefull, it might break down.", 
-                "room1": "", 
-                "room2": ""}
+                "room1": "Room 103. A faded scent of dust lingers in the air. The wallpaper peels revealing stained walls."\
+                 " A flickering ceiling light casts long shadows across the room's furniture." \
+                        "An unmade bed, a cracked vanity mirror, and a creaky armoire half-open.",
+                "room2": "Room 104. The room smells of smoke. A rusted ceiling fan turns slowly above, barely moving the air."\
+                         " Yellowed lace curtains hang over a single, cracked window, filtering the dim light from the street outside."\
+                        " An old rotary phone sits on the nightstand beside a chipped lamp. The bed is lumpy and uneven, its sheets faded."}
+"}
 
 import Lobby as l
 name = input("Hi, what's your name! ")
 print(f"Hi {name}! Thank you for playing my Escpae Room Game!")
-print("The year is 1957. A storm brews over the Atlantic, and the wind howls like a warning through the pines. \n"
-        "You have arrived at the junction of the city and the mountains to visit a friend. \n" 
-        "Suddenly, a building resembling an ancient castle catches your attention. \n"
-        "Coming closer, you see the name —— Cliffside Hotel, \n"
-        "a once-grand retreat for the wealthy elite, now left to decay at the edge of the world. \n" 
-        "Out of curiosity, you walk in. \n" 
-        "The storm slams the door shut behind you with a thunderous crack. \n" 
-        "You try to open the door, but it's locked. \n"
+print("The year is 1957. A storm brews over the Atlantic, and the wind howls like a warning through the pines." \
+        "You have arrived at the junction of the city and the mountains to visit a friend." \
+        "Suddenly, a building resembling an ancient castle catches your attention." \
+        "Coming closer, you see the name —— Cliffside Hotel," \
+        "a once-grand retreat for the wealthy elite, now left to decay at the edge of the world." \
+        "Out of curiosity, you walk in." \
+        "The storm slams the door shut behind you with a thunderous crack." \
+        "You try to open the door, but it's locked." \
         "Find a way to escape the Cliffside Hotel. Good luck!")
 print(descriptions["lobby"])
 l.lobby()
-
 
