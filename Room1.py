e79267ecd854a339ea3709d7cf505c63e1d76106
Iris Xu <yingyan.xu1@rbe.sk.ca>
2025-06-18 07:53:48 -0600

    add documentations for room1 and room2

--- Room1.py
+++ Room1.py
@@ -1,13 +1,30 @@
-# import modules and libraries
+####################################################################################
+#Module Name: Room1
+'''
+Description: Room1 refers to Room103, in which the player will find Key Fragment2,
+a locked box, a sudoku, and an exit door, located at the north, south, east, and west 
+side of the Room103 respectively. Key Fragment2 and Room104 Key can be collected 
+in this room. The product of the answer to the sudoku and the room number, 103 is 
+the password to the locked box. After collecting the items and entering the password, 
+the player will be able to leave the room.
+'''
+####################################################################################
+
 import Move as m
 import Bag as b
 import Room2 as r
 from tabulate import tabulate
+
 answer = 0
 pw = 8
 password = 8*103
 flag = True
 
+
+'''
+Collect Key Fragment2 and place it in the bag.
+The player is not allowed to collect it twice.
+'''
 def north():
     if "Key Fragment2" in b.bag:
         print("Sorry, you have already collected the second key fragment here. Please visit other places.")
@@ -15,6 +32,12 @@ def north():
         print("You collect the second key fragment. You are one step closer to escape.")
         b.bag.append("Key Fragment2")
 
+
+'''
+Located at the south side of Room103 is a locked box. 
+The player needs to solve the sudoku in the east first before being able to open the box 
+to collect Room104 Key. The player is not allowed to collect it twice.
+'''
 def south():
     if "Room104 Key" in b.bag:
         print("You've opened the box and collected Room104 Key. You don't need to do it again.")
@@ -22,10 +45,11 @@ def south():
     
     print("You see a locked box. The password is three-digit. \n"
           "Hint: calculate the product of the two positive integers in this room.")
-    if answer == 0:
+    if answer == 0: # don't let the player enter password without solving the puzzle first
         print("Sorry, you can't open the box without solving the puzzle. Please do it first.")
         return
-
+    
+    #player enters the password
     while True:
         try:
             ans = int(input("Password: "))
@@ -42,8 +66,12 @@ def south():
         except:
             print("Please type a number.")
 
+
+'''A sudoku game, the player needs to find which number "X" represents'''
 def east():
     global answer
+
+    #sudoku, using tabulate for formatting
     print("You see a sudoku: ")
     sudoku = [[None, 2, None, None, 8, None, None, 7, None],
                 [4, 7, None, None, None, 9, None, None, None],
@@ -56,6 +84,7 @@ def east():
                 [6, None, None, None, 9, None, None, 5, 3]]
     print(tabulate(sudoku, tablefmt = "fancy_grid"))
 
+    #player answers "What number does X represent?: "
     while True:
         try:
             answer = int(input("What number does X represent?: "))
@@ -67,8 +96,14 @@ def east():
         except: 
             print("Please type a single digit.")
 
+
+'''
+The door to leave Room103. The player is not allowed to leave without 
+collecting all the items in the room.
+'''
 def west():
     global flag
+    print("This is the door to leave Room103.")
     if "Room104 Key" in b.bag and "Key Fragment2" in b.bag:
         print("You've found all the items in this room. You may leave the room now.")
         flag = False
@@ -79,13 +114,22 @@ def west():
 
 dir = [north, south, east, west]
 
+
+'''
+Main room1 function that gives a description of the Room103 and leads the player
+to different directions by calling the 4 direction methods above.
+'''
 def room1():
+    #room description of Room103
     print()
     print("Now you are in Room 103.")
     print("A faded scent of dust lingers in the air. The wallpaper peels revealing stained walls.\n"
     " A flickering ceiling light casts long shadows across the room's furniture. \n"
     "An unmade bed, a cracked vanity mirror, and a creaky armoire half-open.")
     print()
+
+    # a while loop that keeps asking the player which direction they want to go 
+    # until the player leaves the room.
     while flag:
         direction = m.move()
         dir[direction]()
--- Room2.py
+++ Room2.py
@@ -1,10 +1,28 @@
+####################################################################################
+#Module Name: Room2
+'''
+Description: Room2 refers to Room104, which requires the Room104 Key to enter.
+After entering, the player will only see a locked box. The player needs to use the 
+UV Torch from the bag to see the hidden password inside the room.
+Using the password, the player opens the box, finds the last piece of key fragment,
+and successfully escapes the hotel. 
+'''
+####################################################################################
+
 import Bag as b
 import Countdown as c
 
+'''
+After entering Room104, first step we have a room description. Then the player will be asked to get the bag
+and grab the UV Torch from the bag to see the hidden password in the room. Then he/she will be able to open
+a locked box, find the last piece of key fragment, and escape the hotel.
+'''
 def room2():
+    # room description
     print("The room smells of smoke. A rusted ceiling fan turns slowly above, barely moving the air."\
             " Yellowed lace curtains hang over a single, cracked window, filtering the dim light from the street outside."\
             " An old rotary phone sits on the nightstand beside a chipped lamp. The bed is lumpy and uneven, its sheets faded.")
+    print()
     print("In this room, you only see a locked box with a four-digit password.")
     while True:
         choice = input("What do you do now?: ")
@@ -28,7 +46,8 @@ def room2():
             print("Hint: type out the thing that you have along the way.")
 
 
-# a function that requires the player to use Room104 Key in the bag to open the Room104 door
+
+'''a function that requires the player to use Room104 Key in the bag to open the Room104 door'''
 def entering():
     print("Walking ...")
     c.countdown(2)
@@ -41,7 +60,7 @@ def entering():
             choice = int(input("choice (1/2): "))
             if choice == 1:
                 while True:
-                    b.check()
+                    b.check() # go over the items in the bag
                     print("Which item do you want to use now?")
                     ans = int(input("choose a number: "))
                     if ans > 0 and ans <= len(b.bag):
