fdb2aae5b01b02e1989ca12e44e3482b66a6d29e
Iris Xu <yingyan.xu1@rbe.sk.ca>
2025-06-17 22:38:56 -0600

    add documentations for bag, map, and countdown modules

--- Countdown.py
+++ Countdown.py
@@ -1,5 +1,15 @@
+####################################################################################
+#Module Name: Countdown
+'''
+Description: the countdown timer creates a time interval before the next code segment 
+appears, which works with the code "print("walking...")" to simulate the feeling of 
+the player walking.
+'''
+####################################################################################
+
 import time
 
+'''create a time interval of t seconds before the next code segment appears'''
 def countdown(t):
     while t:
         time.sleep(1)
